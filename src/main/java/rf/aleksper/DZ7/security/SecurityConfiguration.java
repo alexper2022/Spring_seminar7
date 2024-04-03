3f0eec8acf6ed664eb79c9498d7055fef423d533
AlexPer2022 <alexper@list.ru>
2024-04-03 20:04:11 +0300

    GB Spring DZ7 edited security filter chain

--- src/main/java/rf/aleksper/DZ7/security/SecurityConfiguration.java
+++ src/main/java/rf/aleksper/DZ7/security/SecurityConfiguration.java
@@ -4,15 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.authorizeHttpRequests(reg -> reg
+        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(reg -> reg
                         .requestMatchers("ui/**").hasAnyAuthority("user", "admin")
                         .anyRequest().permitAll())
+
                 .formLogin(Customizer.withDefaults())
                 .build();
     }
